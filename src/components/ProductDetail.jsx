40f5b96c71bd0833f96bd25698d1b3be7457006a
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-15 05:38:37 -0700

    Wire up PDP to use data from id param

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,4 +1,26 @@
+import { useParams } from "react-router-dom";
+import products from "../data/product-data.js";
+
 function ProductDetail() {
-  return <div>ProductDetail</div>;
+  const { id } = useParams();
+  const product = products.find((product) => product.id.toString() === id);
+  console.log(product);
+  const tags = product.tags.map((tag) => <li key={tag}>{tag}</li>);
+
+  return (
+    <div>
+      <div>{product.title}</div>
+      <div>{product.type}</div>
+      <div>{product.rarity}</div>
+      <div>{product.price}</div>
+      {product.price < product.compare_at_price && (
+        <div>{product.compare_at_price}</div>
+      )}
+      <div>{product.description}</div>
+      <div>{product.image}</div>
+      <div>{product.available}</div>
+      <ul>{tags}</ul>
+    </div>
+  );
 }
 export default ProductDetail;
