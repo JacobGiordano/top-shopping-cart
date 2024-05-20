835f612ddf9bbf09b444836405643009cac3cf57
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-20 05:27:44 -0700

    Add radix themes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Theme } from "@radix-ui/themes";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import routes from "./routes";
+import "@radix-ui/themes/styles.css";
 
 const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Theme appearance='dark'>
+      <RouterProvider router={router} />
+    </Theme>
   </React.StrictMode>
 );
