c491cae8109701b0f573fbe3935a4486f1042d06
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-20 05:29:39 -0700

    Update collection page to use radix themes & account for "all"

--- src/components/ProductCollection.jsx
+++ src/components/ProductCollection.jsx
@@ -1,20 +1,72 @@
 import { useParams } from "react-router-dom";
 import data from "../data/products.json";
+import {
+  Text,
+  Box,
+  Card,
+  Inset,
+  Grid,
+  Section,
+  Container,
+} from "@radix-ui/themes";
 
 function ProductCollection() {
   const { tags } = useParams();
   const splitTags = tags.split("+");
-  const matches = data.products.filter((product) => {
-    return product.tags.some((tag) => splitTags.includes(tag));
-  });
+  let matches = [];
+  if (tags == "all") {
+    matches = data.products;
+  } else {
+    matches = data.products.filter((product) => {
+      return product.tags.some((tag) => splitTags.includes(tag));
+    });
+  }
 
   console.log(matches);
   return (
-    <div>
-      {matches.map((match) => {
-        return <div key={match.id}>{match.title}</div>;
-      })}
-    </div>
+    <Section>
+      <Container>
+        <Box align='center'>
+          <Grid
+            columns={{
+              xs: "repeat(2, 1fr)",
+              sm: "repeat(3, 1fr)",
+            }}
+            gap='5'
+            rows='auto'
+            width='auto'
+            justify={{ xs: "center" }}
+            align={{ xs: "center" }}
+            maxWidth={"900px"}
+          >
+            {matches.map((match) => {
+              return (
+                <Box key={match.id}>
+                  <Card size='2'>
+                    <Inset clip='padding-box' side='top' pb='current'>
+                      <img
+                        src={match.image}
+                        alt={match.title}
+                        style={{
+                          display: "block",
+                          objectFit: "cover",
+                          width: "100%",
+                          height: "auto",
+                          backgroundColor: "var(--gray-5)",
+                        }}
+                      />
+                    </Inset>
+                    <Text as='p' size='3'>
+                      {match.title}
+                    </Text>
+                  </Card>
+                </Box>
+              );
+            })}
+          </Grid>
+        </Box>
+      </Container>
+    </Section>
   );
 }
 export default ProductCollection;
