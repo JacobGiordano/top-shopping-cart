cae9c0ca195612ed03192506d638f1ba2e269f87
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-22 05:46:49 -0700

    Remove App.css and add homepage image to root

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import "./App.css";
+import { Container, Section } from "@radix-ui/themes";
 import data from "./data/products.json";
 
 function App() {
   return (
     <>
-      <div>Hello World</div>
-      <ul>
-        {data.products.map((product) => {
-          return (
-            <li key={product.id}>
-              <div className='product-card'>
-                <img src={product.image} alt='' className='card-img' />
-                <div className='card-info'>
-                  <span className='card-title'>{product.title}</span>
-                  <span className='price'>{product.price}</span>
-                  {product.compare_at_price !== product.price && (
-                    <span className='compare-at-price'>
-                      {product.compare_at_price}
-                    </span>
-                  )}
-                </div>
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+      <Section>
+        <img src='/images/store-images/homepage-banner.jpg' alt='' />
+      </Section>
     </>
   );
 }
