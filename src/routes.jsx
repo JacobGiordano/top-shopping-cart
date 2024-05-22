138a5caec9b1576ac3e1a279a21ee660da19bc02
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-21 22:55:01 -0700

    Add NotFound to routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import App from "./App";
 import ProductCollection from "./components/ProductCollection";
 import ProductDetail from "./components/ProductDetail";
 import Cart from "./components/Cart";
+import NotFound from "./components/NotFound";
 
 const routes = [
   {
@@ -11,10 +12,12 @@ const routes = [
   {
     path: "collections/:tags",
     element: <ProductCollection />,
+    errorElement: <NotFound />,
   },
   {
     path: "/products/:handle",
     element: <ProductDetail />,
+    errorElement: <NotFound />,
   },
   {
     path: "/cart",
