de76d5a648a9d838d1520df86b7de202ecec04df
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-23 10:11:05 -0700

    Add padding to main Box comp

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import "@radix-ui/themes/styles.css";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Section, Theme } from "@radix-ui/themes";
+import { Box, Section, Theme } from "@radix-ui/themes";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import routes from "./routes";
@@ -12,11 +12,13 @@ const router = createBrowserRouter(routes);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Theme>
-      <Header></Header>
-      <Section>
-        <RouterProvider router={router} />
-      </Section>
-      <Footer></Footer>
+      <Box px='4'>
+        <Header></Header>
+        <Section>
+          <RouterProvider router={router} />
+        </Section>
+        <Footer></Footer>
+      </Box>
     </Theme>
   </React.StrictMode>
 );
