ab8fd7e48cf0f1c491777f00c529a5d220aa1f26
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-24 10:58:07 -0700

    Update text styling for PDP info titles

--- src/components/PDPInfoTitle.jsx
+++ src/components/PDPInfoTitle.jsx
@@ -1,6 +1,10 @@
 import { Text } from "@radix-ui/themes";
 
 function PDPInfoTitle({ text }) {
-  return <Text size='2'>{text}</Text>;
+  return (
+    <Text size='1' weight='medium'>
+      {text}
+    </Text>
+  );
 }
 export default PDPInfoTitle;
