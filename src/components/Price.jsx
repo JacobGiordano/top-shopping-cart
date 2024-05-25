0e903c1c1691014c950550178a12efbd40666e58
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-25 05:10:20 -0700

    Swap gems for coins as currency in Price comp

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,11 +1,13 @@
 import { Flex } from "@radix-ui/themes";
 import Gem from "/src/assets/svg/gem.svg?react";
+import Coins from "/src/assets/svg/coins.svg?react";
 
 function Price({ children }) {
   return (
-    <Flex gap='.5'>
+    <Flex gap='2'>
       {children}
-      <Gem className='w-4' />
+      {/* <Gem className='w-4 -ml-1.5'></Gem> */}
+      <Coins className='w-4 -ml-1.5'></Coins>
     </Flex>
   );
 }
