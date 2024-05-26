f061e94b87d8e6ee9602634391e58b76c57a0252
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-26 05:43:05 -0700

    Define cart state & share with outlets

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,17 +1,19 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Cart from "../routes/Cart";
 import { Section } from "@radix-ui/themes";
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 function Root() {
+  const [cart, setCart] = useState([]);
+  console.log(cart);
   return (
     <>
       <Header />
       <Section>
-        <Outlet />
+        {/* Passing context using object structuring */}
+        <Outlet context={{ cart, setCart }} />
       </Section>
-      <Cart />
       <Footer />
     </>
   );
