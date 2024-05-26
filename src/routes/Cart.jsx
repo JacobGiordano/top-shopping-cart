03e6c1f650fdad1642eeb49969d4d64a33dc6818
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-26 05:44:34 -0700

    Add foundation for displaying line items in Cart page

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,4 +1,22 @@
+import { useOutletContext } from "react-router-dom";
+import { Box } from "@radix-ui/themes";
+
 function Cart() {
-  return <div>Cart</div>;
+  const context = useOutletContext();
+  console.log(context.cart);
+  return (
+    <Box>
+      <div>Cart</div>
+      <ul>
+        {context.cart.map((lineItem) => (
+          <li key={lineItem.id}>
+            {lineItem.title}
+            {" x"}
+            {lineItem.quantity}
+          </li>
+        ))}
+      </ul>
+    </Box>
+  );
 }
 export default Cart;
