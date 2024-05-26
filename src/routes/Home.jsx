7ce1654b76e8c1f8e3318a5287ac83ecf9c1f47e
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-26 05:45:06 -0700

    Add sanity check to ensure cart state persists on homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,8 @@
+import { useOutletContext } from "react-router-dom";
+
 function Home() {
+  const context = useOutletContext();
+  console.log(context.cart);
   return <div>Home</div>;
 }
 export default Home;
