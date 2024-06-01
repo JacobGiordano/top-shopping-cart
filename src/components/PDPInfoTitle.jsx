64851da9a76017927f88a8081436eb8ba8e8ee3c
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-05-31 21:03:33 -0700

    Update PDP info title weight

--- src/components/PDPInfoTitle.jsx
+++ src/components/PDPInfoTitle.jsx
@@ -2,7 +2,7 @@ import { Text } from "@radix-ui/themes";
 
 function PDPInfoTitle({ text }) {
   return (
-    <Text size='1' weight='medium'>
+    <Text size='1' weight='bold'>
       {text}
     </Text>
   );
