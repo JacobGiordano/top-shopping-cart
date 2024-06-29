a0fcf42f925d3f83d9a6e3f51579d32a40dd6677
Jacob Giordano <jacobgiordano.noreply@gmail.com>
2024-06-29 13:41:30 -0700

    Fix Guarantee on mobile

--- src/components/Guarantee.jsx
+++ src/components/Guarantee.jsx
@@ -3,7 +3,7 @@ import { Box, Flex, Heading, Text } from "@radix-ui/themes";
 function Guarantee() {
   return (
     <Flex
-      className='relative mt-12 text-center md:text-right'
+      className='relative mt-12 text-center md:text-right h-96 sm:h-auto overflow-hidden'
       justify={{ initial: "center", sm: "end" }}
       align='center'
     >
@@ -13,17 +13,17 @@ function Guarantee() {
         direction='column'
         className='text-white z-10 absolute p-12 lg:max-w-[75%]'
       >
-        <Heading
-          as='h3'
+        <Text
+          as='span'
           align={{ initial: "center", sm: "right" }}
           weight='medium'
-          className='text-lg sm:text-xl md:text-2xl lg:text-3xl'
+          className='text-md md:text-lg lg:text-xl xl:text-2xl'
         >
           Your coin well-spent with the
-        </Heading>
+        </Text>
         <Heading
           as='h2'
-          className='uncial-antiqua-regular uppercase py-2 md:py-5 text-xl sm:text-3xl md:text-4xl lg:text-5xl w-full'
+          className='uncial-antiqua-regular uppercase py-2 md:py-5 text-lg sm:text-3xl md:text-4xl lg:text-5xl w-full'
           align={{ initial: "center", sm: "right" }}
         >
           Gryphon Guarantee
@@ -43,7 +43,10 @@ function Guarantee() {
         </Text>
       </Flex>
       <Box className='absolute left-0 top-0 z-0 bg-slate-950/50 w-full h-full'></Box>
-      <img className='' src='/images/store-images/epic-battle.jpg'></img>
+      <img
+        className='h-full sm:h-[unset] min-w-fit sm:min-w-[unset]'
+        src='/images/store-images/epic-battle.jpg'
+      ></img>
     </Flex>
   );
 }
